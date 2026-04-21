151a570430361be856b60259362376a28e074f8c
Rafly Julian <rayubandung@gmail.com>
2026-04-21 15:48:47 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('umkm-cache').then((cache) => {
+    return cache.addAll(['/', '/index.html', '/app.js', '/manifest.json']);
+  }));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
