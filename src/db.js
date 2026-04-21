156d90cb58bea64ec963e0d0efb7312cd5c2e992
Rafly Julian <rayubandung@gmail.com>
2026-04-21 15:39:18 +0700

    Delete src directory

--- src/db.js
+++
@@ -1,8 +0,0 @@
-import Dexie from 'dexie';
-
-export const db = new Dexie('CashflowUMKM_DB');
-db.version(1).stores({
-  categories: '++id, name, icon',
-  inventory: '++id, name, categoryId, stock, price',
-  transactions: '++id, type, amount, date, description'
-});
